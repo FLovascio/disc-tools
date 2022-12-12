1aece5e7061e29ac5c3d7718ff7b34027863dba8
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2022-12-12 16:06:25 +0100

    adding some prototypes to path_integrals.py

---
+++ path_integrals.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numba import njit,prange
+from typing import Callable
+
+@njit
+def constant_distance_integral()->np.float64:
+
+def cylindrical_integral()->np.float64:
+
+
+
