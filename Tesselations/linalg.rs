037601659bab2b69c8b75fc409586af1e8c5392a
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2023-01-05 18:07:36 +0100

    some more basic linalg

--- Tesselations/linalg.rs
+++ Tesselations/linalg.rs
@@ -1,13 +1,32 @@
-mod linalg{
-  struct vector<T>{
-    e1: T,
-    e2: T,
-    e3: T,
-  }
-  fn cross<T>(a:vector<T>,b:vector<T>)->vector<T>{
-    return vector<T>(a.e2*b.e3-a.e3*b.e2, a.e3*b.e1-b.e1*a.e3, a.e1*b.e2-a.e2*b.e1);
-  }
-  fn dot<T>(a:vector<T>,b:vector<T>)->T{
-    return a.e1*b.e1+a.e2*b.e2+a.e3*b.e3;
-  }
+use std::num;
+
+pub struct vector<T:Float>{
+  e1: T,
+  e2: T,
+  e3: T,
+}
+
+pub fn cross<T>(a:vector<T>,b:vector<T>)->vector<T>{
+  return vector<T>(a.e2*b.e3-a.e3*b.e2, a.e3*b.e1-b.e1*a.e3, a.e1*b.e2-a.e2*b.e1);
+}
+
+pub fn dot<T>(a:vector<T>,b:vector<T>)->T{
+  return a.e1*b.e1+a.e2*b.e2+a.e3*b.e3;
+}
+
+pub fn minus<T>(a:vector<T>,b:vector<T>)->vector<T>{
+  return vector<T>(a.e1-b.e1, a.e2-b.e2, a.e3-b.e3);
+}
+
+pub fn plus<T>(a:vector<T>,b:vector<T>)->vector<T>{
+  return vector<T>(a.e1+b.e1, a.e2+b.e2, a.e3+b.e3);
+}
+
+pub fn norm<T>(a:vector<T>)->T{
+  return num::sqrt(a.e1*a.e1+ a.e2*a.e2+ a.e3*a.e3);
+}
+
+pub fn direction<T>(a:vector<T>)->vector<T>{
+  vNorm:T=norm(a);
+  return vector<T>(a.e1/vNorm,a.e2/vNorm,a.e3/vNorm)
 }
--- contour_integrals.py
+++ contour_integrals.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numba import njit,prange
 from typing import Callable
+import disc_cuts
 
 @njit
 def spherical_integral(position:np.ndarray,data:np.ndarray,center:np.ndarray,radius:np.float64,phiRange:tuple[np.float64,np.float64])->np.float64:
@@ -10,6 +11,6 @@ def spherical_integral(position:np.ndarray,data:np.ndarray,center:np.ndarray,rad
 @njit
 def cylindrical_integral(position:np.ndarray,data:np.ndarray,center:np.ndarray,radius:np.float64,zRange:tuple[np.float64,np.float64])->np.float64:
   integral:np.float64=np.float64(0.0);
-  for 
+  return integral;
 
 
