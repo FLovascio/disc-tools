0547abd9f820f8fac105fe928624b16db0c881b8
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2023-01-06 17:21:24 +0100

    oct cube added

---
+++ Surfaces/src/cubes.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "linalg.hpp"
+#include "triangles.hpp"
+#include <vector>
+#include <array>
+
+template <typename T>
+class neighbourCube{
+  private:
+    std::array<linalg3D::vector<T>,27> nodes;
+  public:
+  neighbourCube(std::array<linalg3D::vector<T>,27> nodes_):nodes(nodes_){;}
+  auto operator()(int i_, int j_, int k_)->linalg3D::vector<T>&{
+    return nodes[(i_+1)+3*(j_+1)+9*(k_+1)];
+  }
+};
