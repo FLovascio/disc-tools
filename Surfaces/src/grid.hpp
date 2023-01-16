17334c04f591e102a73292896279962e540924a2
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2023-01-16 17:55:51 +0100

    going home

--- Surfaces/src/grid.hpp
+++ Surfaces/src/grid.hpp
@@ -20,10 +20,10 @@ struct grid{
     return dataPoints[i_+(nx*j_)+(nx*ny*k_)];
   }
   inline auto cube(unsigned int i_,unsigned int j_,unsigned int k_)const->dataCube<T>{
-    return dataCube<T>{{{this(i_,j_,k_),this(i_,j_,k_),this(i_,j_,k_),this(i_,j_,k_),
-                                                             this(i_,j_,k_),this(i_,j_,k_),this(i_,j_,k_),this(i_,j_,k_)}}};
+    return dataCube<T>{{{this(i_,j_,k_),this(i_+1,j_,k_),this(i_,j_+1,k_),this(i_+1,j_+1,k_),
+                         this(i_,j_,k_+1),this(i_+1,j_,k_+1),this(i_,j_+1,k_+1),this(i_+1,j_+1,k_+1)}}};
   }
   auto construct_marching_cube(unsigned int i_,unsigned int j_,unsigned int k_)const->marchingCube<T>{
-    return
+    //return
   }
 };
